a502a87db4c8dfb9999b40d1aeede3cb0fb919a8
Nicholas Goertemiller <44421972+ngoert@users.noreply.github.com>
2022-04-19 22:51:24 -0500

    Update MyItemsViewController.swift

--- sharest/sharest/Controller/MyItemsViewController.swift
+++ sharest/sharest/Controller/MyItemsViewController.swift
@@ -6,25 +6,169 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
-class MyItemsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class MyCell: UICollectionViewCell {
+        let imageView = UIImageView()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            addSubview(imageView)
+        }
+        required init?(coder: NSCoder) {
+            fatalError()
+        }
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            imageView.frame = bounds
+        }
     }
+
+
+
+
+
+    class MyItemsViewController: UIViewController, PHPickerViewControllerDelegate, UICollectionViewDataSource {
+
+        private let collectionView: UICollectionView = {
+            let layout = UICollectionViewFlowLayout()
+            layout.itemSize = CGSize(width: 200, height: 200)
+            let pic = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            pic.backgroundColor = .orange
+            pic.register(MyCell.self, forCellWithReuseIdentifier: "cell")
+            return pic
+        }()
+        
+        
+        
+       // @IBOutlet weak var itemImageView: UIImageView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.addSubview(collectionView)
+            collectionView.dataSource = self
+            collectionView.frame = view.bounds
+            title = "My Items"
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+           
+            // Do any additional setup after loading the view.
+        }
+        
+        // Add gradients
+        override func viewWillAppear(_ animated: Bool) {
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = self.view.bounds
+            gradientLayer.colors = [
+                UIColor.systemOrange.cgColor,
+                UIColor.systemPink.cgColor,
+            ]
+                                        
+            self.view.layer.insertSublayer(gradientLayer, at: 0)
+        }
+        
+        @objc private func didTapAdd() {
+            var config = PHPickerConfiguration(photoLibrary: .shared())
+            config.selectionLimit = 0
+            config.filter = .images
+            let vc = PHPickerViewController(configuration: config)
+            vc.delegate = self
+            present(vc, animated: true)
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            
+            picker.dismiss(animated: true, completion: nil)
+            let group = DispatchGroup()
+            
+            results.forEach { result in
+                group.enter()
+                result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] reading, error in defer {
+                    group.leave()
+                }
+                    guard let image = reading as? UIImage, error == nil else{
+                        return
+                    }
+                    self?.images.append(image)
+                }
+            }
+            
+            group.notify(queue: .main) {
+                print(self.images.count)
+                self.collectionView.reloadData()
+            }
+            
+        }
+        
+        private var images = [UIImage]()
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return images.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? MyCell else { fatalError() }
+            cell.imageView.image = images[indexPath.row]
+            return cell
+        }
+        
+        //collectionView.reloadData()
+    }
+        
+        
+        /*
+        // Add gradients
+        override func viewWillAppear(_ animated: Bool) {
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = self.view.bounds
+            gradientLayer.colors = [
+                UIColor.systemOrange.cgColor,
+                UIColor.systemPink.cgColor,
+            ]
+                                        
+            self.view.layer.insertSublayer(gradientLayer, at: 0)
+        }
+        
+        
+        @IBAction func didTapMe() {
+            let vc = UIImagePickerController()
+            vc.sourceType = .photoLibrary
+            vc.delegate = self
+            vc.allowsEditing = true
+            present(vc, animated: true)
+        }
+    }
+        
+        extension MyItemsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+            
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                
+                if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+                    itemImageView.image = image
+            }
+                
+                picker.dismiss(animated: true, completion: nil)
+            }
+            
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                picker.dismiss(animated: true, completion: nil)
+            }
+        }
+
+        /*
+        // MARK: - Navigation
+
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
+    */
+
+
     
-    // Add gradients
-    override func viewWillAppear(_ animated: Bool) {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = [
-            UIColor.systemOrange.cgColor,
-            UIColor.systemPink.cgColor,
-        ]
-                                    
-        self.view.layer.insertSublayer(gradientLayer, at: 0)
-    }
+
 
     /*
     // MARK: - Navigation
@@ -36,4 +180,3 @@ class MyItemsViewController: UIViewController {
     }
     */
 
-}
